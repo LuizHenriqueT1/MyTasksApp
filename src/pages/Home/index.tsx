941717ad616dd48ee9feb1440d6b53847259dae6
LuizHenriqueT1 <luiz.hesoaress@gmail.com>
2023-10-22 18:47:01 -0300

    Adicionando FlatList para Scroll das tarefas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import {
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+} from 'react-native';
+
+export function Home() {
+  interface Task {
+    id: string;
+    title: string;
+  }
+
+  const [newTask, setNewTask] = React.useState('');
+  const [tasks, setTasks] = React.useState<Task[]>([]);
+
+  const hadleAddNewTask = () => {
+    const data = {
+      id: String(new Date().getTime()),
+      title: newTask ? newTask : 'Task empty',
+    };
+
+    setTasks([...tasks, data]);
+  };
+
+  return (
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Dev!</Text>
+        <TextInput
+          onChangeText={setNewTask}
+          placeholderTextColor="#555"
+          placeholder="Nova tarefa..."
+          style={styles.input}></TextInput>
+        <TouchableOpacity
+          onPress={hadleAddNewTask}
+          activeOpacity={0.7}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.titleTasks}>Minhas Tarefas</Text>
+
+        <FlatList
+          data={tasks}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <TouchableOpacity style={styles.buttonTask}>
+              <Text style={styles.titleTask}>{item.title}</Text>
+            </TouchableOpacity>
+          )}
+        />
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: '#121214',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#121214',
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+  title: {
+    color: '#F1F1F1',
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  titleTasks: {
+    color: '#F1F1F1',
+    fontSize: 23,
+    fontWeight: 'bold',
+    marginVertical: 50,
+  },
+  input: {
+    backgroundColor: '#29292e',
+    color: '#f1f1f1',
+    padding: Platform.OS == 'ios' ? 15 : 12,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#eba417',
+    padding: Platform.OS == 'ios' ? 15 : 12,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#121214',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonTask: {
+    backgroundColor: '#29292e',
+    padding: 10,
+    marginTop: 10,
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  titleTask: {
+    color: '#f1f1f1',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
